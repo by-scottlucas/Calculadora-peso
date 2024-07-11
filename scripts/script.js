b14aa31e4262d21ae1ed67961ca81b89927026a4
Lucas S <Lucasluke307@gmail.com>
2024-07-11 20:45:05 -0300

    Aplicando media queries para desktops

--- scripts/script.js
+++ scripts/script.js
@@ -13,7 +13,7 @@ function calcularPesoIdeal() {
     const pesoMin = sexoSelecionado === 'feminino' ? altura - 110 : altura - 100;
     const pesoMax = pesoMin + 15;
 
-    const resultadoTexto = `Seu peso ideal é ${pesoIdeal}kg. No entanto, continua sendo considerado saudável se variar entre ${pesoMin}kg e ${pesoMax}kg.<br><br> 
+    const resultadoTexto = `Seu peso ideal é <strong>${pesoIdeal} kg</strong>. No entanto, continua sendo considerado saudável se variar entre <strong>${pesoMin}kg</strong> e <strong>${pesoMax}kg</strong>.<br><br> 
                             Caso você sinta que este resultado não se adequa ao seu biotipo, é aconselhado que busque aconselhamento individual com um nutricionista ou um profissional de educação física, 
                             por exemplo, para fazer outras medições mais objetivas, como uma bioimpedância, para entender qual o peso que melhor se adequa para você.`;
 
@@ -21,7 +21,10 @@ function calcularPesoIdeal() {
     const textoResultado = document.getElementById("results-text");
 
     textoResultado.innerHTML = resultadoTexto;
-    divResultado.style.display = "block";
-    divResultado.scrollIntoView({ behavior: 'smooth' });
+    divResultado.style.display = "flex";
 
+}
+
+function fecharModal() {
+    document.getElementById('results').style.display = "none";
 }
